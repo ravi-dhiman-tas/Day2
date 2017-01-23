d17836038664bdb09c1a471db9f1d60c64c149b5
ravid7000 <ravi@technoarchsoftwares.com>
2017-01-23 21:01:49 +0530

    Routers added

--- src/AppModule.ts
+++ src/AppModule.ts
@@ -1,10 +1,27 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent } from './app/app.component';
+//  core packages
+import { NgModule }         from '@angular/core';
+import { BrowserModule }    from '@angular/platform-browser';
+import { RouterModule, Routes }     from "@angular/router";
+
+// components
+import { AppComponent }     from './app/app.component';
+import { PageNotFoundComponent }     from './app/PageNotFound.component';
+
+
+export const appRoutes: Routes = [
+    { path: '', component: PageNotFoundComponent },
+    { path: 'any', component: PageNotFoundComponent }
+];
 
 @NgModule({
-    imports: [BrowserModule],
-    declarations: [AppComponent],
+    imports: [
+        BrowserModule,
+        RouterModule.forRoot(appRoutes)
+    ],
+    declarations: [
+        AppComponent,
+        PageNotFoundComponent
+    ],
     bootstrap: [AppComponent]
 })
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,14 @@
+// cores
 import { Component } from "@angular/core";
 import { NgModule } from "@angular/core";
 
 @Component({
     selector: 'my-app',
-    template: '<h1>Hello World</h1>'
+    template: `
+        <h1>HTML from template.</h1>
+        <a routerLink="/any" routerLinkActive="active">New page</a>
+        <router-outlet></router-outlet>
+    `
 })
 
 export class AppComponent {}
